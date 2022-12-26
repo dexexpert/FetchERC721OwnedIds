996103aff8899429969c59d2d23d8017904f8a6f
t1nyTi <94951233+t1nyTi@users.noreply.github.com>
2022-12-27 03:02:59 +0700

    Updated Smart Contract Address Input

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
   const [ownerAddress, setOwnerAddress] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [showTokens, setShowTokens] = useState('');
+  const [smartContractAddress, setSmartContractAddress] = useState('');
   const tokensOfOwner = async ({ contractAddress, walletAddress }) => {
     setIsLoading(true);
     let url = 'https://bscrpc.com';
@@ -70,7 +71,7 @@ export default function App() {
   const fetchIds = async () => {
     const owned = await tokensOfOwner({
       // contractAddress: '0xe3b1d32e43ce8d658368e2cbff95d57ef39be8a6',
-      contractAddress: '0x9292eecf86ac8633af9e67ddf10e8eff92bf5632',
+      contractAddress: smartContractAddress,
       walletAddress: ownerAddress,
     });
     console.log(owned);
@@ -83,12 +84,26 @@ export default function App() {
         This App is to fetch token IDs owned by address
       </h1>
       <p>Input your address here and just click fetch :)</p>
-      <input
-        value={ownerAddress}
-        onChange={(e) => {
-          setOwnerAddress(e.target.value);
-        }}
-      ></input>
+      <div>
+        <input
+          value={smartContractAddress}
+          style={{ width: '300px' }}
+          placeholder="Smart Contract Address"
+          onChange={(e) => {
+            setSmartContractAddress(e.target.value);
+          }}
+        ></input>
+      </div>
+      <div style={{ marginTop: '20px', marginBottom: '20px' }}>
+        <input
+          value={ownerAddress}
+          style={{ width: '300px' }}
+          placeholder="Owner Address"
+          onChange={(e) => {
+            setOwnerAddress(e.target.value);
+          }}
+        ></input>
+      </div>
       <button style={{ marginLeft: '10px' }} onClick={fetchIds}>
         fetch
       </button>
